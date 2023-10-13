163220f96db644c137de243036418da2a185859a
Mo'Khaled Bin-Hazem <Occ716528@gmail.com>
2023-10-13 23:52:47 +0300

    Add Page Links

---
+++ Assets/js/links.js
@@ -0,0 +1,20 @@
+let amount = 0;
+
+let divElemnts = document.querySelector('.links-continer');
+var liElements = divElemnts.querySelectorAll('li');
+
+function privuosLink(){
+    debugger
+    if(amount > -(liElements.length-25)*(100/liElements.length)){
+        amount -=100/liElements.length;
+        divElemnts.querySelectorAll('ul')[0].style.transform=`translateX(${amount}%)`;
+    }  
+}
+
+function nextLink(){
+
+    if(amount<0){
+        amount +=100/liElements.length;
+        divElemnts.querySelectorAll('ul')[0].style.transform=`translateX(${amount}%)`;
+    }
+}
